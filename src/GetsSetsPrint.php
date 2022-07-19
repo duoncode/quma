c59f315c35f5b1e5e0e2a2a0b2252049f223230f
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2022-07-19 19:21:31 +0200

    Add GetsSetsPrint trait

---
+++ src/GetsSetsPrint.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Conia\Puma;
+
+trait GetsSetsPrint
+{
+    protected bool $print;
+
+    public function print(bool $print = false): bool
+    {
+        // Normally this is bad practise but setting print should
+        // only be used for debugging purposes
+        if (func_num_args() > 0) {
+            $this->print = $print;
+        }
+
+        return $this->print;
+    }
+}
